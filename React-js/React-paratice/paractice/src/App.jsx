acfef3fc5465317f9bcad5c57a7ebf88c20f9eeb
kashifjavaid1 <gmdcf797@gmail.com>
2025-04-19 23:12:44 +0500

    react-toggleShow

--- React-js/React-paratice/paractice/src/App.jsx
+++ React-js/React-paratice/paractice/src/App.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react"
+import ToggleShow from "./component/ToggleShow"
 
 function App() {
-  const [show,setShow]=useState(false)
+  const [show,setShow]=useState(true)
 // const fun=(num1,num2,total)=>{
 //   total=num1+num2;
 //   alert(total)
@@ -11,9 +12,10 @@ function App() {
     <>
      {/* <button onClick={()=>fun(10,10)}>click</button> */}
 
-     <input type={show?"text":'password'} />
-
-     <button onClick={()=>setShow(!show)}>show password</button>
+     {/* <input type={show?"text":'password'} />
+     <button onClick={()=>setShow(!show)}>show password</button> */}
+     <button onClick={()=>setShow(!show)}>Show Toggle</button>
+     {show?<ToggleShow/>:null}
     </>
   )
 }
---
+++ React-js/React-paratice/paractice/src/component/ToggleShow.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ToggleShow = () => {
+    return (
+        <div>
+            <h1>M.Kashif Javaid</h1>
+        </div>
+    );
+}
+
+export default ToggleShow;
