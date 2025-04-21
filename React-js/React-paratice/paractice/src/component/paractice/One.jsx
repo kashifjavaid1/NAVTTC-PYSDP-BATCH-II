0a419348916af7bdab6a87ee523a34c86f43f52d
kashifjavaid1 <gmdcf797@gmail.com>
2025-04-21 21:57:53 +0500

    onchange-paractie

--- React-js/React-paratice/paractice/src/component/paractice/One.jsx
+++ React-js/React-paratice/paractice/src/component/paractice/One.jsx
@@ -2,11 +2,22 @@ import React, { useState } from 'react';
 
 const One = () => {
     const [data,setData]=useState()
+    const [emailVal,setEmailVal]=useState();
+    const [addressVal,setAddressVal]=useState()
+    const cle=()=>{
+        setData("")
+        setEmailVal("")
+        setAddressVal("")
+    }
     return (
-        <div>
-           <input type="text" value={data} placeholder='Enter Name' onChange={(e)=>setData(e.target.value)} style={{margin:'10px'}}/>
-           <h1>{data}</h1>
-           <button onClick={()=>setData('')}>clear</button>
+        <div style={{margin:'20px',display:'flex',flexDirection:"column",width:'200px'}}>
+           <input type="text" value={data} placeholder='Enter Name' onChange={(e)=>setData(e.target.value)} />
+           <input type="email" value={emailVal} placeholder='Email' onChange={(e)=>setEmailVal(e.target.value)} style={{marginTop:'10px'}}/>
+           <input type="text" value={addressVal} placeholder='Address' onChange={(e)=>setAddressVal(e.target.value)} style={{marginTop:'10px'}}/>
+           <h1>Name:{data}</h1>
+           <h1>Email:{emailVal}</h1>
+           <h1>Address:{addressVal}</h1>
+           <button onClick={cle}>clear</button>
         </div>
     );
 }
