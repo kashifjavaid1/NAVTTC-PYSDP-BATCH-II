38cb0891f8c81293535e45a384eb4f007e018f04
kashifjavaid1 <gmdcf797@gmail.com>
2025-04-22 23:45:57 +0500

    checkbox

--- React-js/React-paratice/paractice/src/component/Parent.jsx
+++ React-js/React-paratice/paractice/src/component/Parent.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import One from './paractice/One';
+import CheckBox from './paractice/CheckBox';
 
 const Parent = () => {
     return (
         <div>
             <One/>
+            <CheckBox/>
         </div>
     );
 }
---
+++ React-js/React-paratice/paractice/src/component/paractice/CheckBox.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+
+const CheckBox = () => {
+    const [skills,setSkills]=useState([])
+
+    const skillFun=(event)=>{
+if (event.target.checked) {
+    setSkills([...skills,event.target.value])
+}
+else{
+    setSkills([...skills.filter((item)=>item!=event.target.value)])
+}
+    }
+    return (
+        <>
+        <div>
+            <h1>Check the Skill's</h1>
+            <input type="checkbox"  id='php' value='php' onChange={skillFun} />
+            <label htmlFor="php">Php</label>
+            <br />
+            <input type="checkbox" id='node' value="node" onChange={skillFun} />
+            <label htmlFor="node">Node</label>
+            <br />
+            <input type="checkbox" id='react' value="react" onChange={skillFun} />
+            <label htmlFor="react">React</label>
+            <br />
+            <input type="checkbox" id='js' value='js' onChange={skillFun} />
+            <label htmlFor="js">Js</label>
+        </div>
+        <h1>{skills.toString()}</h1>
+        </>
+    );
+}
+
+export default CheckBox;
