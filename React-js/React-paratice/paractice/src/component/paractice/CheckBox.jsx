898fc979a7fedc7e81d73558cb01e397cf0d9995
kashifjavaid1 <gmdcf797@gmail.com>
2025-04-26 22:59:32 +0500

    react-paractice

--- React-js/React-paratice/paractice/src/component/paractice/CheckBox.jsx
+++ React-js/React-paratice/paractice/src/component/paractice/CheckBox.jsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 const CheckBox = () => {
     const [skills,setSkills]=useState([])
 
+    // skills check and unchecked
     const skillFun=(event)=>{
 if (event.target.checked) {
     setSkills([...skills,event.target.value])
