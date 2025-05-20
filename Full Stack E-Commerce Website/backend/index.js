63826653e764f02e62ac752026823c3b440ed495
kashifjavaid1 <gmdcf797@gmail.com>
2025-05-20 22:26:15 +0500

    e-commerce

---
+++ Full Stack E-Commerce Website/backend/index.js
@@ -0,0 +1,10 @@
+import express from 'express'
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hi! My Name is Kashif')
+});
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
