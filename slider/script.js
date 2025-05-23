f9ca071053f7aff3390f3ec7714b73cdf57406a6
kashifjavaid1 <gmdcf797@gmail.com>
2025-05-23 22:25:32 +0500

    slider

---
+++ slider/script.js
@@ -0,0 +1,5 @@
+const btn=document.querySelectorAll("btn-container");
+const slider=document.querySelector("slider");
+const slide=document.querySelectorAll("slide");
+let i=0;
+slide[i].classList.add("active");
